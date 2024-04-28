13b5682880bc36c34383dd8ff2cfe1038e57b158
Balde <abdourahamane620@gmail.com>
2024-04-28 08:31:34 +0200

    Configuration cors policy

--- CorsConfigurations/Program.cs
+++ CorsConfigurations/Program.cs
@@ -7,6 +7,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy
+   (
+    "corspolicy", build => build.AllowAnyOrigin().
+                                AllowAnyHeader().
+                                AllowAnyMethod()
+   );
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +28,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("corspolicy");
 
 app.UseAuthorization();
 
